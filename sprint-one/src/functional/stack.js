19b7546387f865d5ef858931702f25ae8c249997
Erzhena Shirabon <erzhena.shirabon@gmail.com>
2019-04-10 17:24:06 -0700

    finished functional/stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -5,14 +5,36 @@ var Stack = function() {
   var storage = {};
 
   // Implement the methods below
+  var key = 0;
   someInstance.push = function(value) {
+    key++;
+    storage[JSON.stringify(key)] = value;
+    return storage;
   };
 
   someInstance.pop = function() {
+    var val = storage[JSON.stringify(key)]; 
+    delete storage[JSON.stringify(key)];
+    key--;
+    return val;
   };
 
   someInstance.size = function() {
+    var emptyObj = {};
+    if (JSON.stringify(storage) !== JSON.stringify(emptyObj)) {
+      var keys = Object.keys(storage);
+      return keys.length;
+    } else {
+      return 0;
+    }
   };
 
   return someInstance;
 };
+
+
+var myStack = Stack();
+console.log(myStack.push('a'));
+console.log(myStack.push('b'));
+myStack.pop();
+myStack.pop();
