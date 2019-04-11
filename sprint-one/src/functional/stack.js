56197a2b7c262e5986b7c955d49346bc616247cd
Erzhena Shirabon <erzhena.shirabon@gmail.com>
2019-04-10 22:24:20 -0700

    modified version of stack.js

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,38 +3,27 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
-  // Implement the methods below
   var key = 0;
+
   someInstance.push = function(value) {
     key++;
-    storage[JSON.stringify(key)] = value;
+    storage[key] = value;
     return storage;
   };
 
   someInstance.pop = function() {
-    var val = storage[JSON.stringify(key)]; 
-    delete storage[JSON.stringify(key)];
+    if (key === 0) {
+      return;
+    }
+    var val = storage[key];
+    delete storage[key];
     key--;
     return val;
   };
 
   someInstance.size = function() {
-    var emptyObj = {};
-    if (JSON.stringify(storage) !== JSON.stringify(emptyObj)) {
-      var keys = Object.keys(storage);
-      return keys.length;
-    } else {
-      return 0;
-    }
+    return key;
   };
 
   return someInstance;
 };
-
-
-var myStack = Stack();
-console.log(myStack.push('a'));
-console.log(myStack.push('b'));
-myStack.pop();
-myStack.pop();
