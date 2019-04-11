aea5fb49ecd42f800bcaa08c977ec352d225fbcf
Erzhena Shirabon <erzhena.shirabon@gmail.com>
2019-04-11 14:06:02 -0700

    modified SA

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -2,37 +2,45 @@ var Stack = function() {
   var myStack = {};
   myStack.storage = {};
   myStack.key = 0;
-  myStack.push = push;
-  myStack.pop = pop;
-  myStack.size = size;
+  myStack.push = stackMethods.push;
+  myStack.pop = stackMethods.pop;
+  myStack.size = stackMethods.size;
+  extend(myStack, stackMethods);
   return myStack;
 };
 
+
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
+}
+
 var stackMethods = {};
 
-var push = function(val) {
+stackMethods.push = function(val) {
   this.key++;
   this.storage[this.key] = val;
 };
 
-var pop = function() {
+stackMethods.pop = function() {
   if (this.key === 0) {
     return;
   }
   var result = this.storage[this.key];
   delete this.storage[this.key];
-  this.key--;
+  this.key = this.key - 1;
   return result;
 };
 
-var size = function() {
+stackMethods.size = function() {
   return this.key;
 };
 
-var callFunc = Stack();
-console.log(callFunc.size());
-callFunc.push('a');
-callFunc.push('b');
-console.log(callFunc.size());
-//callFunc.pop()
-//callFunc.size()
+// var callFunc = Stack();
+// console.log(callFunc.size());
+// callFunc.push('a');
+// callFunc.push('b');
+// console.log(callFunc.size());
+// //callFunc.pop()
+// //callFunc.size()
